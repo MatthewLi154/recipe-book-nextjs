4541aa2919723967389448bceafa757293542ead
MatthewLi154 <matthewli154@gmail.com>
2023-04-08 22:16:29 -0700

    home page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <img src="https://i.pinimg.com/564x/04/9b/b7/049bb76d791ec7ecf0d14a64efe99fbe.jpg"></img>
         <img src="https://i.pinimg.com/564x/d6/29/c9/d629c9f1e5bbb45c1b884e02fda8d11f.jpg"></img>
         <div className="flex font-black w-200 justify-center items-center text-xl mx-20 h-20 w-50 px-3 bg-slate-300">
-          I love Flayu!
+          I love Flayu!!
         </div>
         {/* <img src="https://media.discordapp.net/attachments/1055580714587926538/1092990283165225050/Selfie_20230404_Taeoxo_000.jpg?width=1202&height=676"></img> */}
       </div>

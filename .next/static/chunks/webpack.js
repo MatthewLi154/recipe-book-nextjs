c55be99e84d59e06486c45e0a6f8522a0a7b851f
MatthewLi154 <matthewli154@gmail.com>
2023-06-05 15:09:09 -0700

    button change register form

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "472f40732c71710c"; }
+/******/ 		__webpack_require__.h = function() { return "3ac453602678406e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
